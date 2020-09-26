868a659e3c6577888e6613d7890172ee3addc708
Derek Parson <parson.derek@gmail.com>
2020-09-25 18:21:11 -0700

    Add API call to get data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,13 +6,21 @@
 </template>
 
 <script>
-
+import axios from 'axios';
 import Main from '../components/Main.vue'
 
 export default {
   name: 'Home',
   components: {
     Main
+  },
+  async created() {
+    const { data } = await axios.get(`https://api.covidtracking.com/v1/us/daily.json`)
+    console.log(data);
   }
 }
 </script>
+
+<style scoped>
+
+</style>
