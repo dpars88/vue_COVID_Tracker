711572b0c54b19453a0d04b3b3cbd0a851081df7
Derek Parson <parson.derek@gmail.com>
2020-09-29 17:41:33 -0700

    Complete API call, create new object with date and corresponding information

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,16 +7,50 @@
 
 <script>
 import axios from 'axios';
-import Main from '../components/Main.vue'
+import Main from '../components/Main.vue';
+import moment from 'moment';
 
 export default {
   name: 'Home',
   components: {
     Main
   },
+  data() {
+    return {
+      arrDeath: [],
+      arrDeathIncrease: [],
+      arrTotalTestResults: [],
+      arrPositive: [],
+      arrNegative: [],
+      arrRecovered: []
+    }
+  },
   async created() {
     const { data } = await axios.get(`https://api.covidtracking.com/v1/us/daily.json`)
-    console.log(data);
+    //console.log(data);
+
+    data.forEach(d => {
+      const date = moment(d.date, "YYYYMMDD").format("MMDD");
+
+      const {
+        death,
+        deathIncrease,
+        totalTestResults,
+        positive,
+        negative,
+        recovered
+      } = d;
+
+      this.arrDeath.push({date, total: death});
+      this.arrDeathIncrease.push({date, total: deathIncrease});
+      this.arrTotalTestResults.push({date, total: totalTestResults});
+      this.arrPositive.push({date, total: positive});
+      this.arrNegative.push({date, total: negative});
+      this.arrRecovered.push({date, total: recovered});
+
+    });
+
+    //console.log(this.arrDeath);
   }
 }
 </script>
